9644e82dd41ea914c4c3625666e277e25e57a5a8
Ujjanth-Arhan-NEU <114028166+Ujjanth-Arhan-NEU@users.noreply.github.com>
2024-04-20 10:39:03 -0400

    Initialized inii file in the module by import module, removed cache folder

--- importmodule.py
+++ importmodule.py
@@ -1,12 +1,12 @@
-from my_pdf_extractor.extractor import *
+import my_pdf_extractor
 
 try:
     # info = extract_info_from_file("C:\Disk_D\CourseWork\Intro to Programming for DS\Project\Resumes\Colon_2024 Resume.pdf")
     # print(type(info))
-    info = extract_info_from_folder("C:\Disk_D\CourseWork\Intro to Programming for DS\Project\Resumes")
+    info = my_pdf_extractor.extract_info_from_folder("Resumes")
     print(info)
     csv_file_path = 'resume_info.csv'
-    write_dict_to_csv(info, csv_file_path)
+    my_pdf_extractor.write_dict_to_csv(info, csv_file_path)
     print(f"CSV file '{csv_file_path}' has been created.")
 
 except Exception as e:
---
+++ my_pdf_extractor/__init__.py
@@ -0,0 +1,1 @@
+from my_pdf_extractor.extractor import *
